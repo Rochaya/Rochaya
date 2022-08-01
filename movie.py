01959b1b8ff529c079c435591d51e2a90b4742d0
[Rochaya] <[olivier.g77580@gmail.com]>
2022-08-02 01:48:22 +0200

    Step 1

---
+++ movie.py
@@ -0,0 +1,40 @@
+import os
+import json
+import logging
+#Import du fichier json
+CUR_DIR = os.path.dirname(__file__)
+DATA_FILE = os.path.join(CUR_DIR, "data", "movies.json")
+
+
+
+# Creation de la classe
+class Movie:
+    def __init__(self, title):
+        self.title = title.title()
+    
+    def __str__(self):
+        return self.title
+
+# Lecture et ecriture du fichier contenant les films   
+    def _get_movies(self):
+            with open(DATA_FILE, "r") as f:
+                return json.load(f)         
+            
+    def _write_movies(self, movies):
+        with open(DATA_FILE, "w") as f:
+                json.dump(movies, f, indent=4)
+
+# Ajout et suppression des films
+    def add_to_movies(self):
+        movies = self._get_movies()
+        if self.title not in movies:
+            movies.append(self.title)
+            self._write_movies(movies)
+            return True
+        else:
+            logging.warning(f"{self.title} est deja dans la liste")
+            return False
+
+if __name__ == "__main__":
+    m = Movie("Chouf")
+    m.add_to_movies()
